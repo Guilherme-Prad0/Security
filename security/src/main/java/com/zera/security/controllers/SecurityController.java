08b4b060b091b10c905cca4988669979ca92052d
Guilherme <guilhermetprado@gmail.com>
2026-05-06 20:30:21 -0300

    Up

--- security/src/main/java/com/zera/security/controllers/SecurityController.java
+++ security/src/main/java/com/zera/security/controllers/SecurityController.java
@@ -3,11 +3,8 @@ package com.zera.security.controllers;
 import com.zera.security.models.SecurityModel;
 import com.zera.security.services.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
