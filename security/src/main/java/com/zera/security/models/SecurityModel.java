92c90b96d54dcc0ffb33d0444cbda3161a392eb5
Guilherme <guilhermetprado@gmail.com>
2026-05-06 19:55:26 -0300

    primeira parte feita

--- security/src/main/java/com/zera/security/models/SecurityModel.java
+++ security/src/main/java/com/zera/security/models/SecurityModel.java
@@ -1,41 +1,16 @@
 package com.zera.security.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "TBL_")
+@Data
 public class SecurityModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String descricao;
-    private String quantidade;
+    private String nome;
 
-    public SecurityModel() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(String quantidade) {
-        this.quantidade = quantidade;
-    }
 }
